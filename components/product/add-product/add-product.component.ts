13b7d4beac9b45a68855a76cd5f9569c7808ce2d
RakkehAmeni <75304059+RakkehAmeni@users.noreply.github.com>
2021-01-14 12:59:51 +0100

    Add files via upload

---
+++ components/product/add-product/add-product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+
+@Component({
+  selector: "app-add-product",
+  templateUrl: "./add-product.component.html",
+  styleUrls: ["./add-product.component.scss"],
+})
+export class AddProductComponent implements OnInit {
+  
+
+  
+  constructor(
+
+  ) {}
+
+  ngOnInit() {
+    
+  }
+
+  
+}
